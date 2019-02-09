04f9dd3973cc197ac67a17eb146067039b070a87
Blade3575 <blade3575@gmail.com>
2019-02-08 19:52:21 -0500

    Initial commit.
    Initial commit.

---
+++ CSMemEdit/Memory.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CSMemEdit
+{
+    class Memory
+    {
+        private IntPtr proc { get; set; }
+
+        public Memory() { }
+
+        public Memory(int procId)
+        {
+            this.proc = Win32.OpenProcess(Win32.ProcessAccessRights.PROCESS_ALL_ACCESS, false, procId);
+            if (this.proc == IntPtr.Zero)
+            {
+                var error = Marshal.GetLastWin32Error();
+                if (error == 5)
+                    throw new Exception(string.Format("ERROR: {0} try running as admin.", error));
+                else
+                    throw new Exception(string.Format("ERROR: {0}", error));
+            }
+        }
+
+        ~Memory()
+        {
+            Win32.CloseHandle(this.proc);
+        }
+
+        public IntPtr GetProcessHandle()
+        {
+            return proc;
+        }
+
+        public IntPtr GetProcessModuleHandle(string moduleName)
+        {
+            var hMods = new IntPtr[1024];
+
+            var gch = GCHandle.Alloc(hMods, GCHandleType.Pinned);
+            var pModules = gch.AddrOfPinnedObject();
+
+            var uiSize = (uint)(Marshal.SizeOf(typeof(IntPtr)) * (hMods.Length));
+
+            if (Win32.EnumProcessModules(proc, pModules, uiSize, out uint cbNeeded) == 1)
+            {
+                var uiTotalNumberofModules = (Int32)(cbNeeded / (Marshal.SizeOf(typeof(IntPtr))));
+
+                for (var i = 0; i < (int)uiTotalNumberofModules; ++i)
+                {
+                    var stringBuilder = new StringBuilder(1024);
+
+                    Win32.GetModuleFileNameEx(proc, hMods[i], stringBuilder, (uint)(stringBuilder.Capacity));
+                    if (Path.GetFileName(stringBuilder.ToString().ToLower()) == moduleName.ToLower())
+                        return hMods[i];
+                }
+            }
+
+            gch.Free();
+
+            return IntPtr.Zero;
+        }
+
+        public byte[] ReadProcMem(int procId, IntPtr addressToRead, int lengthToRead)
+        {
+            if (this.proc == IntPtr.Zero)
+                this.proc = Win32.OpenProcess(Win32.ProcessAccessRights.PROCESS_ALL_ACCESS, false, procId);
+
+            return ReadProcMem(addressToRead, lengthToRead);
+        }
+
+        public byte[] ReadProcMem(IntPtr addressToRead, int lengthToRead)
+        {
+            if (!Win32.VirtualProtectEx(this.proc, addressToRead, (UIntPtr)lengthToRead, Win32.MemoryProtectionConstants.PAGE_READONLY, out Win32.MemoryProtectionConstants oldProtect))
+                throw new Exception(string.Format("ERROR: {0}", Marshal.GetLastWin32Error().ToString()));
+
+            var buffer = new byte[lengthToRead];
+
+            if (!Win32.ReadProcessMemory(this.proc, addressToRead, buffer, lengthToRead, out IntPtr bufferRead))
+                throw new Exception(string.Format("ERROR: {0}",  Marshal.GetLastWin32Error().ToString()));
+
+            return buffer;
+        }
+
+        public int WriteProcMem(IntPtr procHandle, IntPtr addressToWrite, byte[] bytesToWrite)
+        {
+            if (this.proc == IntPtr.Zero)
+                this.proc = Win32.OpenProcess(Win32.ProcessAccessRights.PROCESS_ALL_ACCESS, false, procHandle.ToInt32());
+
+            return WriteProcMem(addressToWrite, bytesToWrite);
+        }
+
+        public int WriteProcMem(IntPtr addressToWrite, byte[] bytesToWrite)
+        {
+            var newProtect = Win32.MemoryProtectionConstants.PAGE_READWRITE;
+            if (!Win32.VirtualProtectEx(this.proc, addressToWrite, (UIntPtr)bytesToWrite.Length, newProtect, out Win32.MemoryProtectionConstants oldProtect))
+                return 0;
+
+            if (!Win32.WriteProcessMemory(this.proc, addressToWrite, bytesToWrite, bytesToWrite.Length, out int bufferWritten))
+                return 0;
+
+            if (!Win32.VirtualProtectEx(this.proc, addressToWrite, (UIntPtr)bytesToWrite.Length, oldProtect, out newProtect))
+                return 0;
+
+            return bufferWritten;
+        }
+    }
+}
---
+++ CSMemEdit/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace CSMemEdit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var mem = new Memory(Process.GetCurrentProcess().Id);
+                var modHandle = mem.GetProcessModuleHandle("CSMemEdit.exe");
+
+                if (modHandle == IntPtr.Zero)
+                    Console.WriteLine("ERROR: Failed to get module handle.");
+
+                Win32.MODULEINFO mi = new Win32.MODULEINFO();
+                var result = Win32.GetModuleInformation(mem.GetProcessHandle(), modHandle, out mi, (uint)Marshal.SizeOf(mi));
+
+                if (!result)
+                    Console.WriteLine("ERROR: " + Marshal.GetLastWin32Error().ToString());
+
+                Console.WriteLine("Memory ranges from 0x{0} to 0x{1}.", mi.lpBaseOfDll.ToString("X8"), ((uint)mi.lpBaseOfDll + mi.SizeOfImage).ToString("X8"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
